8e1f9e4b8b59a0ce7f5cef3ae83c6bbe123cbb25
mattcp1980 <matthewparker@diversitus.com>
2020-11-27 18:18:47 +0000

    Updated to protect password in tests - moved to env variable

--- cypress/integration/automationpractice/Login.spec.js
+++ cypress/integration/automationpractice/Login.spec.js
@@ -1,4 +1,5 @@
 // <reference types="cypress" />
+//NOTE running or opening cypress will need to be prefixed with CYPRESS_password=****** where ****** is the password
 
 describe('Login to the site', () => {
     beforeEach(() => {
@@ -7,6 +8,7 @@ describe('Login to the site', () => {
 
         //pull in user fixture data
         cy.fixture('users.json').as('users')
+        
     })
     it('includes a sign in link', () => { 
         cy.get('.login').contains('Sign in')
@@ -16,13 +18,18 @@ describe('Login to the site', () => {
         cy.contains('Authentication')
     })
     it('logs the user in when entering valid details', function() {
+        //pass in password as an environment variable
+        const password = Cypress.env('password')
+        if (typeof password !== 'string' || !password) {
+            throw new Error('Missing password value, set using CYPRESS_password=...')
+        }
         //loop through the users to check multiple users logging in
         cy.get('@users').each((user) =>{
 
             //load the login page and enter each users details from the fixture file
             cy.get('.login').click()
             cy.get('#email').type(user.email)
-            cy.get('#passwd').type('mat28par')
+            cy.get('#passwd').type(password, {log:false})
             cy.get('#SubmitLogin > span').click()
             
             //check that the first name and last name are displayed correctly for each user
@@ -63,11 +70,15 @@ describe('Login to the site', () => {
         })
     })
     it('checks that UNKNOWN email provides the correct validation', () => {
-
+        //pass in password as an environment variable
+        const password = Cypress.env('password')
+        if (typeof password !== 'string' || !password) {
+            throw new Error('Missing password value, set using CYPRESS_password=...')
+        }
         //load the login page and enter the first user login details
         cy.get('.login').click()
         cy.get('#email').type('mattp1980invalid@virginmedia.com')
-        cy.get('#passwd').type('mat28par')
+        cy.get('#passwd').type(password, {log:false})
         cy.get('#SubmitLogin > span').click()
 
         //check that the Authentication failed message is displayed
@@ -75,11 +86,15 @@ describe('Login to the site', () => {
         
     })
     it('checks that INVALID email provides the correct validation', () => {
-
+        //pass in password as an environment variable
+        const password = Cypress.env('password')
+        if (typeof password !== 'string' || !password) {
+            throw new Error('Missing password value, set using CYPRESS_password=...')
+        }
         //load the login page and enter the first user login details
         cy.get('.login').click()
         cy.get('#email').type('invaliduser')
-        cy.get('#passwd').type('mat28par')
+        cy.get('#passwd').type(password, {log:false})
         cy.get('#SubmitLogin > span').click()
 
         //check that the Authentication failed message is displayed
--- cypress/integration/automationpractice/Search.spec.js
+++ cypress/integration/automationpractice/Search.spec.js
@@ -1,18 +1,24 @@
 // <reference types="cypress" />
 describe('Complete an product search', () => {
     beforeEach(() => {
+        //set password to env variable password passed in and ensure if not passed in errors don't show the password.
+        //NOTE running or opening cypress will need to be prefixed with CYPRESS_password=****** where ****** is the password
+        const password = Cypress.env('password')
+        if (typeof password !== 'string' || !password) {
+            throw new Error('Missing password value, set using CYPRESS_password=...')
+        }
         //login, load the page and pull the product fixture data in
         cy.login('matthewparker@diversitus.com', 'mat28par')
         cy.pageload()
         cy.fixture('products.json').as('products')
     })
-    it('Enters a single value into search search using search button', () =>{
+    it('enters a general term into search and searches using search button', () =>{
         cy.get('#search_query_top').type('Dress')
         cy.get('#searchbox > .btn').click()
         cy.contains('No results were found for your search').should('not.exist')
         cy.get('.lighter').contains('Dress')
     })
-    it('Checks specific products searched return correct products', function() {
+    it('checks specific products searched return correct products', function() {
         //Loop through each product in the collection
         cy.get('@products').each((product) =>{
             //Enter the product and run a search
